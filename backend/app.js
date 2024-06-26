1ea147e0834963b3ce420a3d34d6ab2633069cec
gabysenac <maria.gbenjamim@senacsp.edu.br>
2024-06-26 17:08:42 -0300

    alfala

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import routerLivros from './routes/livrosRouter.js'
+import routerFavorito from './routes/favoritosRouter.js'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors({origin: "*"}))
+
+const port = 8000
+
+// Montagem do roteador "livros"
+app.use('/livros', routerLivros);
+
+// Montagem do roteador "favoritos"
+app.use('/favoritos', routerFavorito);
+
+// Iniciar o servidor
+app.listen(port, () =>{
+    console.log(`Escutando na porta ${port}`)
+})
